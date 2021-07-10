8c632ba754a85e80825fb9428d3e0fa8b2f948d4
Sean Lowe <sean.lowe@a2zsync.com>
2021-07-09 23:10:03 -0600

    try to add dotenv, still WIP

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -4,16 +4,17 @@ import chalk = require('chalk');
 import clear = require('clear');
 import figlet = require('figlet');
 import program = require('commander');
-// import path = require('path');
-
+import * as dotenv from 'dotenv';
+// custom module imports
 import greeter = require('./modules/greet');
 import weather = require('./modules/weather');
 import purpose = require('./modules/purpose');
 import wiki = require('./modules/wiki');
 
-// -------------
-
+// clear out terminal window
 clear();
+
+// print banner
 console.log(
     chalk.cyan(
         figlet.textSync('NOX-CLI', { horizontalLayout: 'full' })
@@ -21,22 +22,30 @@ console.log(
 );
 
 program
-    .version('0.0.3')
+    .version('0.0.4.5')
     .description("SSLv2's attempt at a basic version of JARVIS")
-    .option('-s, --search <string>', 'search for something')
+    .option('-s, --search <string>', 'Search for something')
+    .option('-w, --weather', 'Query the weather API')
     .option('-t, --tinker', 'Boot into tinker mode')
-
 program.parse(process.argv);
 
 const options = program.opts();
 
+dotenv.config();
+console.log(process.env.OPEN_WEATHER_API_KEY);
+
 greeter.greetUser();
-if (Object.keys(options).length !== 0) {
-    if (Object.keys(options).includes('search')) {
+let keys = Object.keys(options);
+if (keys.length !== 0) {
+    // convert to loop over arguments?
+    // remove arguments as they are hit?
+    if (keys.includes('search')) {
         wiki.search(options['search']);
     }
+    if (keys.includes('weather')) {
+        weather.weatherReport();
+    }
     // purpose.determineUseCase();
-
 } else {
     console.log("No options supplied!");
 }
--- typescript/src/modules/weather.ts
+++ typescript/src/modules/weather.ts
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
-export function weatherReport() {
-    console.log("hello!");
+import * as process from 'process';
+import axios from 'axios';
+
+export async function weatherReport() {
+    console.log(process.env.OPEN_WEATHER_API_KEY);
+    // get city
+    const city = 'Denver';
+    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`
+
+    // const response = await axios.get(url);
+    console.log("some phony response data");
 }
