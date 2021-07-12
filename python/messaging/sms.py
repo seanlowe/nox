1f6207cdfd31378f26f8ac8102af5d6afe771c75
Sean Lowe <sean.lowe@a2zsync.com>
2021-07-12 12:15:09 -0600

    start sms in ts

--- python/messaging/sms.py
+++ python/messaging/sms.py
@@ -13,37 +13,45 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-email = "scttsnl@gmail.com"
-pas = "sean0313"
+email = "email@email.com"
+pas = "password"
 
-# sms_gateway = '9196499812@txt.att.net' # sean
-sms_gateway = '7203175427@messaging.sprintpcs.com' # kennen
+number = "<phone number>" # add logic to programmatically replace this
+gateway = '@messaging.sprintpcs.com'
+
+sms = number + gateway
+
 # The server we use to send emails in our case it will be gmail but every email provider has a different smtp 
 # and port is also provided by the email provider.
 smtp = "smtp.gmail.com" 
 port = 587
+
 # This will start our email server
 server = smtplib.SMTP(smtp,port)
+
 # Starting the server
 server.starttls()
+
 # Now we need to login
 server.login(email,pas)
 
 # Now we use the MIME module to structure our message.
 msg = MIMEMultipart()
 msg['From'] = "NOX"
-msg['To'] = sms_gateway
+msg['To'] = sms
+
 # Make sure you add a new line in the subject
-# msg['Subject'] = "Test Message\n"
+msg['Subject'] = "Test Message\n"
+
 # Make sure you also add new lines to your body
-body = "Hi Kennen!\n\nThis is Sean sending you a message from rudimentary NOX messaging systems.\n\nTotally cool but totally useless!"
-# body = "<html><br/><p>This is a text message from rudimentary NOX messaging systems.</p><br/><p>Please do not respond to this message.</p></html>"
+body = "This is a message from rudimentary NOX messaging systems.\n\nTotally cool but totally useless!"
+
 # and then attach that body furthermore you can also send html content.
 msg.attach(MIMEText(body, 'html'))
 
-sms = msg.as_string()
+msg = msg.as_string()
 
-server.sendmail(email,sms_gateway,sms)
+server.sendmail(email,sms,msg)
 
 # lastly quit the server
 server.quit()
---
+++ typescript/src/modules/sms.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+export function sms(msg: string, number: string) {
+    // can we send SMS / emails through SMTP like in python?
+}
