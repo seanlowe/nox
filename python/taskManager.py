48c6bd2d6c274bb1a2f014738a80b4c549f1e425
Sean Lowe <sean.lowe@a2zsync.com>
2021-08-21 16:16:07 -0600

    start taskManager/hub idea

---
+++ python/taskManager.py
@@ -0,0 +1,16 @@
+
+
+# sockets?
+
+
+
+# listen for incoming request
+request = ''
+
+if (request):
+    # start new task in background
+    print("Hello!")
+else:
+    # keep working background tasks
+    print("Later!")
+
