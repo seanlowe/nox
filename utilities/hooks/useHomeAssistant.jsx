849e064e670a7843ab82180e6ac332032a03218c
Sean Lowe <sean.lowe@a2zsync.com>
2022-06-13 17:26:32 -0600

    create hooks and contexts dirs

---
+++ utilities/hooks/useHomeAssistant.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export const useHomeAssistant = () => {
+  const [ state, setState ] = useState( 'Loading' )
+
+  useEffect(() => {
+    let interval
+    function checkStatus() {
+      interval = setTimeout( async () => {
+        const { data: response } = await axios.get( '/api/hass' )
+        setState( response.hassStatus )
+        checkStatus()
+      }, 10000 )
+    }
+
+    checkStatus()
+    return () => {
+      clearTimeout( interval )
+    }
+  }, [] )
+
+  return state
+}
