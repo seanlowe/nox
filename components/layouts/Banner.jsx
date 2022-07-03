cce1183a1071015422c3b15ec825ccd8d14d19ad
Sean Lowe <sean.lowe@a2zsync.com>
2022-07-02 18:59:54 -0600

    update banner to be more responsive

--- components/layouts/Banner.jsx
+++ components/layouts/Banner.jsx
@@ -2,31 +2,32 @@ import { useContext } from 'react'
 import Image from 'next/image'
 import PropTypes from 'prop-types'
 import StatusContext from '../../utilities/contexts/StatusContext'
+import { useMediaQuery } from 'react-responsive'
+
 
 const Banner = ({ name }) => {
-	const { state } = useContext( StatusContext )
+  const { state } = useContext( StatusContext )
+  const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
 
-	return (
-		<div className='horizon'>
-			<Image
-				priority
-				src={state.indicator}
-				className='borderCircle'
-				height={100}
-				width={175}
-				alt={name}
-			/>
-			<div className='banner-content'>
-				<center>
-					<span className='headline'>{name}</span>
-				</center>
-			</div>
-		</div>
-	)
+  return (
+    <div className='horizon'>
+      {!isMobile && (
+        <Image
+          priority
+          src={state.indicator}
+          className='banner-image'
+          height={100}
+          width={175}
+          alt={name}
+        />
+      )}
+      <span className='headline'>{name}</span>
+    </div>
+  )
 }
 
 Banner.propTypes = {
-	name: PropTypes.string,
+  name: PropTypes.string,
 }
 
 export default Banner
