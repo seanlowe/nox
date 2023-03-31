d09c672809c6c7299c5d6c57d21f2686d54e86ad
Sean Lowe <scttsnl@gmail.com>
2023-03-30 22:02:15 -0600

    be: add global db connection
    Adds a global variable as a handle to the database and defers closing that connection until the program closes.
    
    uses upper/db library and adapter for postgresql

--- noxd.go
+++ noxd.go
@@ -7,13 +7,33 @@ import (
   "os"
 
   "nox/noxd/routes"
+  "nox/noxd/globals"
 
   "github.com/joho/godotenv"
   "github.com/go-chi/chi/v5"
+  "github.com/upper/db/v4/adapter/postgresql"
 )
 
-func main() {
+func init() {
+  fmt.Println("Initializing NOX . . . ")
+
   err := godotenv.Load(".env")
+
+  DATABASE_URL := os.Getenv("DATABASE_URL_UPPER")
+  connURL, err := postgresql.ParseURL(DATABASE_URL)
+	if err != nil {
+		log.Fatal("ParseURL: ", err)
+	}
+
+  sess, err := postgresql.Open(connURL)
+  if err != nil {
+    log.Fatal("Open: ", err)
+  }
+
+	globals.DbConn = sess
+}
+
+func main() {
   GO_PORT := os.Getenv("BACKEND_PORT")
 
   router := chi.NewRouter()
@@ -22,7 +42,9 @@ func main() {
   routes.InitRoutes(router)
 
   fmt.Printf("starting server at port %s\n", GO_PORT)
-  if err = http.ListenAndServe(fmt.Sprintf(":%s", GO_PORT), router); err != nil {
+  if err := http.ListenAndServe(fmt.Sprintf(":%s", GO_PORT), router); err != nil {
     log.Fatal(err)
   }
+
+  defer globals.DbConn.Close()
 }
---
+++ noxd/globals/globals.go
@@ -0,0 +1,7 @@
+package globals
+
+import (
+	"github.com/upper/db/v4"
+)
+
+var DbConn db.Session
