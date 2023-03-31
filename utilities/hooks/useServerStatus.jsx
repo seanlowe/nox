0b1e25cff885f1b3a6a439d52abcad7f1b8a721c
Sean Lowe <scttsnl@gmail.com>
2023-03-30 22:11:41 -0600

    fe: rename hook to be more generic
    update API call to new golang backend

---
+++ utilities/hooks/useServerStatus.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import { SERVER_STATE } from '../../services/react/StatusService'
+import backendApi from '../instances/axios'
+
+export const useServerStatus = ( name ) => {
+  const [ state, setState ] = useState( SERVER_STATE.LOADING )
+
+  useEffect(() => {
+    let interval
+    function checkStatus() {
+      interval = setTimeout( async () => {
+        const { data: serverState } = await backendApi.get( `/status/${name}` )
+        setState( serverState )
+        checkStatus()
+      }, 15000 )
+    }
+
+    checkStatus()
+    return () => {
+      clearTimeout( interval )
+    }
+  }, [] )
+
+  return state
+}
