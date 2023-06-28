89f19dc90d16c6580017b61e3bc7da5554eaf60e
Sean Lowe <35589586+seanlowe@users.noreply.github.com>
2023-06-27 22:15:13 -0600

    admin: simplify install script

--- utilities/sh/install.sh
+++ utilities/sh/install.sh
@@ -1,134 +1,74 @@
 #!/bin/bash
 
+if [ "$SHELL" != "/bin/bash" ] ; then
+  echo "Please run this script with bash."
+  exit 1
+fi
+
 MISSING_COMPONENTS=''
+RUNNING_DIR=$(dirname "$(readlink -f "$0")")
+BIN_DIR="$RUNNING_DIR/../../bin"
 
 # color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # no color
-# LIGHT_GREY_BACKGROUND='\033[0;100m'
-# NORMAL_BACKGROUND='\033[0;49m'
 
 # unicode characters
 INSTALL_FOUND="$GREEN\U2714$NC"
 INSTALL_MISSING="$RED\U2716$NC"
 INSTALL_SEARCHING="\U2754"
-WAVING_HAND="\U1F44B"
 WRENCH="\U1F527"
 SPARKLES="\U2728"
 GLOWING_STAR="\U1F31F"
 
+function validateInstall() {
+  local program=$1
+  local dash=$2
+
+  echo -en "$INSTALL_SEARCHING $program"
+  eval "$(${program} ${dash}version &> /dev/null)"
+
+  if [[ $? -ne 0 ]] ; then
+    sleep .5
+    echo -e "\r $INSTALL_MISSING"
+    MISSING_COMPONENTS="$MISSING_COMPONENTS $program"
+    return 1
+  fi
+
+  sleep .5
+  echo -e "\r $INSTALL_FOUND"
+
+  return 0
+}
+
 echo "Checking for required dependencies:"
 
-# check for installation of node and npm
-echo -en "$INSTALL_SEARCHING Node"
-node --version &> /dev/null
+validateInstall "node" "--"
 
-# if node is not installed, add node to list of missing components
-if [[ $? -ne 0 ]] ; then
-  sleep .5
-  echo -e "\r $INSTALL_MISSING"
-  MISSING_COMPONENTS="$MISSING_COMPONENTS node"
-else
-  sleep .5
-  echo -e "\r $INSTALL_FOUND"
-fi
+validateInstall "npm" "--"
 
-echo -en "$INSTALL_SEARCHING NPM"
-npm --version &> /dev/null
+validateInstall "go" ""
 
-if [[ $? -ne 0 ]] ; then
-  sleep .5
-  echo -e "\r $INSTALL_MISSING"
-  MISSING_COMPONENTS="$MISSING_COMPONENTS npm"
-else
-  sleep .5
-  echo -e "\r $INSTALL_FOUND"
-fi
+validateInstall "docker" "--"
 
-# check for installation of go
-echo -en "$INSTALL_SEARCHING Go"
-go version &> /dev/null
-
-# if go is not installed, add go to list of missing components
-if [[ $? -ne 0 ]] ; then
-  sleep .5
-  echo -e "\r $INSTALL_MISSING"
-  MISSING_COMPONENTS="$MISSING_COMPONENTS go"
-else
-  sleep .5
-  echo -e "\r $INSTALL_FOUND"
-fi
-
-# check for installation of docker
-echo -en "$INSTALL_SEARCHING Docker"
-docker --version &> /dev/null
-
-# if docker is not installed, add docker to list of missing components
-if [[ $? -ne 0 ]] ; then
-  sleep .5
-  echo -e "\r $INSTALL_MISSING"
-  MISSING_COMPONENTS="$MISSING_COMPONENTS docker"
-else
-  sleep .5
-  echo -e "\r $INSTALL_FOUND"
-fi
-
-# check for installation of homebrew
-echo -en "$INSTALL_SEARCHING Homebrew"
-brew --version &> /dev/null
-
-# if homebrew is not installed, quit with a message
-if [[ $? -ne 0 ]] ; then
-  sleep .5
-  echo -e "\r $INSTALL_MISSING"
-  MISSING_COMPONENTS="$MISSING_COMPONENTS brew"
-else 
-  sleep .5
-  echo -e "\r $INSTALL_FOUND"
-fi
-
-# check for installation of overmind
-# if brew is missing, don't try to check for overmind
-BREW="brew"
-if [[ $MISSING_COMPONENTS == *"$BREW"* ]] ; then
-  MISSING_COMPONENTS="$MISSING_COMPONENTS overmind"
-else
-  echo -en "$INSTALL_SEARCHING Overmind"
-  overmind --version &> /dev/null
-
-  # if overmind is not installed but brew is, install it
-  if [ $? -ne 0 ]; then
-    sleep .5
-    echo -en "\r $INSTALL_MISSING Overmind ... installing"
-    brew install overmind
-
-    echo $?
-
-    if [ $? -ne 0 ] ; then
-      sleep 1
-      echo -e "\r $INSTALL_MISSING Overmind ... install failed."
-      MISSING_COMPONENTS="$MISSING_COMPONENTS overmind"
-    else
-      sleep 1
-      echo -e "\r $INSTALL_FOUND"
-    fi
-  else
-    sleep 1
-    echo -e "\r $INSTALL_FOUND"
-  fi
-fi
+validateInstall "overmind" "--"
 
 # display missing components
 if [[ $MISSING_COMPONENTS != "" ]] ; then
   echo -e "\nMissing components: $MISSING_COMPONENTS"
-  echo "Please install the missing components and try again."
+  echo "Please install the missing components and try again. Read the manual installation instructions for more information. (docs/manual-installation.md)"
   exit 1
 fi
 
 # else, continue with the installation
 echo -e "\nAll required dependencies are installed! $GLOWING_STAR\n"
 echo -e "Continuing with installation... $WRENCH\n"
+
+
+# ------------------- #
+#       FRONTEND      #
+# ------------------- #
 
 # starts in /home/slowe/Documents/projects/nox
 echo -e "Installing the Nox frontend (Next.js)"
@@ -139,6 +79,10 @@ if [[ $? -ne 0 ]] ; then
   echo -e " $INSTALL_MISSING Frontend install failed. Please try again.\n"
   exit 1
 fi
+
+# ------------------- #
+#       BACKEND       #
+# ------------------- #
 
 echo -e "Installing the Nox backend  (golang)"
 cd ../noxd
@@ -157,6 +101,10 @@ cp .env.example .env
 echo -e "\n.env file created! Don't forget to update your ENV values."
 
 echo -e "\nNox installation complete! $SPARKLES\n"
+
+# ------------------- #
+#     INSTRUCTIONS    #
+# ------------------- #
 
 # display instructions for starting the app
 echo -e "To start the app, run the following command:"
